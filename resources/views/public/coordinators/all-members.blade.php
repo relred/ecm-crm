5188f55f589f83c2ac70900b726ce1ace60658eb
Omar <jesusomarizabal@gmail.com>
2025-06-20 16:50:05 -0700

    added cover image

--- resources/views/public/coordinators/all-members.blade.php
+++ resources/views/public/coordinators/all-members.blade.php
@@ -7,6 +7,9 @@
     <script src="https://cdn.tailwindcss.com"></script>
 </head>
 <body class="bg-gray-100">
+    <div>
+        <img src="{{ asset('images/cover.jpeg') }}" alt="Cover" class="w-full">
+    </div>
     <div class="min-h-screen">
         <header class="bg-white shadow">
             <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
